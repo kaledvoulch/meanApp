8703da175542d33df76f86cbc8c96b7e448d00ae
kaledvoulch <theandrewduffin@gmail.com>
2019-01-08 11:54:34 +0000

    the first commit

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const config = require('./config/databse');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+try {
+  mongoose.set('userNewUrlParser', true);
+  mongoose.set('useCreateIndex', true);
+  mongoose.connect(config.database);
+}
+catch(e){
+  console.log(e.message);
+}
 
-var app = express();
+// add route for our api
+const api = require('./routes/api');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
+
+// add Cors Support before any routing
+app.use( fn: function(reg, res, next)) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Expose-Headers', 'Authorization'); // have to expose so that browsers have
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    next();
+}
+
+app.use(passport.initalize());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +48,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+app.use(fn: '/api', api);
+app.use(fn: '/users', users);
+
+app.get('/', function(reg, res) {
+  res.send('Page under construction');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
